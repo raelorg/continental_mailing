deea582ac1e0122e552cce5a92eb9772b684c475
raelorg <69607164+raelorg@users.noreply.github.com>
2022-04-26 15:08:06 -0400

    Avoid sending notifications from SATGING, etc.

--- Elohim.net.php
+++ Elohim.net.php
@@ -147,8 +147,14 @@ class cElohimNet
    // ---------------------------------------------------------
    public function cron_execution_inactive()
    {
-      $this->unsubscriber_report();
-      $this->inactive_report();
+      $site_url = get_site_url();
+
+      if (  ( $site_url === 'https://raelcanada.org/' ) ||
+            ( $site_url === 'https://raelmexico.org/' ) ||
+            ( $site_url === 'https://raelusa.org/' ) ) {
+         $this->unsubscriber_report();
+         $this->inactive_report();
+      }
    }
 
    // --------------------------------------------------------------------------------
